76dfc56aa97d3f5c3510d5528060d150d170c5a0
Zenige <zenigecarefully@gmail.com>
2021-05-29 13:09:14 +0700

    pull

--- lib/bottomsheet/wordcardEdit.dart
+++ lib/bottomsheet/wordcardEdit.dart
@@ -10,12 +10,10 @@ class WordCardEditFormPage extends StatefulWidget {
   WordCardEditFormPage({Key key, this.deleteID}) : super(key: key);
   @override
   WordCardEditFormPageState createState() => WordCardEditFormPageState(deleteID);
-
 }
 
 class WordCardEditFormPageState extends State<WordCardEditFormPage> {
-
-  WordCardEditFormPageState(this.deleteID);
+   WordCardEditFormPageState(this.deleteID);
   //Database
   final dbHelper = DatabaseHelper.instance;
 
@@ -23,7 +21,7 @@ class WordCardEditFormPageState extends State<WordCardEditFormPage> {
   List<bool> groupSelected = [false, false, false, false, false];
   String wordCardName = '';
   String wordCardTopic = '';
-  int deleteID;
+  int deleteID ;
 
   //Function
   void insertWordCard() async {
@@ -45,116 +43,134 @@ class WordCardEditFormPageState extends State<WordCardEditFormPage> {
   }
 
   @override
-  Widget build(BuildContext context,) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('EDIT WORDCARD'),
       ),
-      body: Material(
-        child: Container(
-          padding: EdgeInsets.fromLTRB(0, 35, 0, 35),
-          color: Colors.white,
-          child: Column(
-            children: <Widget>[
-              Text(
-                'Edit WordCard',
-                style: TextStyle(
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Material(
+          child: Container(
+            padding: EdgeInsets.fromLTRB(0, 35, 0, 35),
+            color: Colors.white,
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Edit WordCard',
+                  style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Kanit-Light'),
                 ),
-              ),
-             
-              SizedBox(height: 8),
-              Text(
-                  '----------------------------------------------------------------'),
-              SizedBox(height: 8),
-              Container(
-                padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
-                child: Column(
-                  children: [
-                    Text('WordCard Name:'),
-                    TextField(
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
+                Container(
+                  padding: const EdgeInsets.fromLTRB(8, 50, 8, 8),
+                  child: Column(
+                    children: [
+                     
+                     TextField(
+                        decoration: new InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                            borderRadius: BorderRadius.all(
+              Radius.circular(10.0)),
+                          ),
+                          
+                          hintText: 'Enter your WordCard Name',
+                        ),
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontFamily: 'Kanit-Light'),
+                        onChanged: (text) {
+                          setState(() {
+                            wordCardName = text;
+                          });
+                        },
                       ),
-                      onChanged: (text) {
-                        setState(() {
-                          wordCardName = text;
-                        });
-                      },
-                    ),
-                    SizedBox(height: 16),
-                    Text('WordCard Topic:'),
-                    TextField(
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
+                      SizedBox(height: 16),
+      
+                      TextField(
+                        decoration: new InputDecoration(
+                          
+                          enabledBorder: OutlineInputBorder(
+                            
+                            borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                             borderRadius: BorderRadius.all(
+              Radius.circular(10.0)),
+                          ),
+                          hintText: 'Enter your WordCard Topic',
+                        ),
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontFamily: 'Kanit-Light'),
+                        onChanged: (text) {
+                          setState(() {
+                            wordCardTopic = text;
+                          });
+                        },
                       ),
-                      onChanged: (text) {
-                        setState(() {
-                          wordCardTopic = text;
-                        });
-                      },
-                    ),
-                    SizedBox(height: 16),
-                    Text('Favorite Group:'),
-                    SizedBox(height: 8),
-                    ToggleButtons(
-                      children: <Widget>[
-                        Icon(Icons.star_rate_rounded,
-                            color: Color(0xFFFF3377), size: 30),
-                        Icon(Icons.star_rate_rounded,
-                            color: Color(0xFFE53344), size: 30),
-                        Icon(Icons.star_rate_rounded,
-                            color: Color(0xFF33DDAA), size: 30),
-                        Icon(Icons.star_rate_rounded,
-                            color: Color(0xFF3344AA), size: 30),
-                        Icon(Icons.star_rate_rounded,
-                            color: Color(0xFFFFDD00), size: 30),
-                      ],
-                      onPressed: (int index) {
-                        setState(() {
-                          groupSelected[index] = !groupSelected[index];
-
-                          print(groupSelected);
-                        });
-                      },
-                      isSelected: groupSelected,
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 16),
-              ElevatedButton.icon(
-                onPressed: () {
-                  if (wordCardName != '' && wordCardTopic != '') {
-                    insertWordCard();
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text(
-                          'Congret! You have $wordCardName as a new WordCard.'),
-                    ));
-                    Navigator.pop(context);
-                  }
-                },
-                icon: Icon(Icons.save),
-                label: Text('Save'),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.green.shade600,
-                  padding: EdgeInsets.symmetric(horizontal: 55, vertical: 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  textStyle: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                      SizedBox(height: 16),
+                      Text('Favorite Group:'),
+                      SizedBox(height: 8),
+                      ToggleButtons(
+                        children: <Widget>[
+                          Icon(Icons.star_rate_rounded,
+                              color: Color(0xFFFF3377), size: 30),
+                          Icon(Icons.star_rate_rounded,
+                              color: Color(0xFFE53344), size: 30),
+                          Icon(Icons.star_rate_rounded,
+                              color: Color(0xFF33DDAA), size: 30),
+                          Icon(Icons.star_rate_rounded,
+                              color: Color(0xFF3344AA), size: 30),
+                          Icon(Icons.star_rate_rounded,
+                              color: Color(0xFFFFDD00), size: 30),
+                        ],
+                        onPressed: (int index) {
+                          setState(() {
+                            groupSelected[index] = !groupSelected[index];
+      
+                            print(groupSelected);
+                          });
+                        },
+                        isSelected: groupSelected,
+                      ),
+                    ],
                   ),
                 ),
-              )
-            ],
+                SizedBox(height: 16),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    if (wordCardName != '' && wordCardTopic != '') {
+                      insertWordCard();
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text(
+                            'Congret! You have $wordCardName as a new WordCard.',style: TextStyle(
+                        
+                            fontFamily: 'Kanit-Light'),),
+                      ));
+                      Navigator.pop(context);
+                    }
+                  },
+                  icon: Icon(Icons.save),
+                  label: Text('Save' ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.lime,
+                    padding: EdgeInsets.symmetric(horizontal: 55, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    textStyle: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/bottomsheet/wordcardInput.dart
+++ lib/bottomsheet/wordcardInput.dart
@@ -44,127 +44,129 @@ class WordCardInputFormPageState extends State<WordCardInputFormPage> {
       appBar: AppBar(
         title: Text('ADD NEW WORDCARD'),
       ),
-      body: Material(
-        child: Container(
-          padding: EdgeInsets.fromLTRB(0, 35, 0, 35),
-          color: Colors.white,
-          child: Column(
-            children: <Widget>[
-              Text(
-                'Create new WordCard',
-                style: TextStyle(
-                    fontSize: 26,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Kanit-Light'),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(8, 50, 8, 8),
-                child: Column(
-                  children: [
-                   
-                   TextField(
-                      decoration: new InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                          borderRadius: BorderRadius.all(
-            Radius.circular(10.0)),
-                        ),
-                        
-                        hintText: 'Enter your WordCard Name',
-                      ),
-                      style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                          fontFamily: 'Kanit-Light'),
-                      onChanged: (text) {
-                        setState(() {
-                          wordCardName = text;
-                        });
-                      },
-                    ),
-                    SizedBox(height: 16),
-
-                    TextField(
-                      decoration: new InputDecoration(
-                        
-                        enabledBorder: OutlineInputBorder(
+      body: SingleChildScrollView(
+        child: Material(
+          child: Container(
+            padding: EdgeInsets.fromLTRB(0, 35, 0, 35),
+            color: Colors.white,
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Create new WordCard',
+                  style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Kanit-Light'),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(8, 50, 8, 8),
+                  child: Column(
+                    children: [
+                     
+                     TextField(
+                        decoration: new InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                            borderRadius: BorderRadius.all(
+              Radius.circular(10.0)),
+                          ),
                           
-                          borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                           borderRadius: BorderRadius.all(
-            Radius.circular(10.0)),
+                          hintText: 'Enter your WordCard Name',
                         ),
-                        hintText: 'Enter your WordCard Topic',
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontFamily: 'Kanit-Light'),
+                        onChanged: (text) {
+                          setState(() {
+                            wordCardName = text;
+                          });
+                        },
                       ),
-                      style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                          fontFamily: 'Kanit-Light'),
-                      onChanged: (text) {
-                        setState(() {
-                          wordCardTopic = text;
-                        });
-                      },
-                    ),
-                    SizedBox(height: 16),
-                    Text('Favorite Group:'),
-                    SizedBox(height: 8),
-                    ToggleButtons(
-                      children: <Widget>[
-                        Icon(Icons.star_rate_rounded,
-                            color: Color(0xFFFF3377), size: 30),
-                        Icon(Icons.star_rate_rounded,
-                            color: Color(0xFFE53344), size: 30),
-                        Icon(Icons.star_rate_rounded,
-                            color: Color(0xFF33DDAA), size: 30),
-                        Icon(Icons.star_rate_rounded,
-                            color: Color(0xFF3344AA), size: 30),
-                        Icon(Icons.star_rate_rounded,
-                            color: Color(0xFFFFDD00), size: 30),
-                      ],
-                      onPressed: (int index) {
-                        setState(() {
-                          groupSelected[index] = !groupSelected[index];
-
-                          print(groupSelected);
-                        });
-                      },
-                      isSelected: groupSelected,
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 16),
-              ElevatedButton.icon(
-                onPressed: () {
-                  if (wordCardName != '' && wordCardTopic != '') {
-                    insertWordCard();
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text(
-                          'Congret! You have $wordCardName as a new WordCard.',style: TextStyle(
-                      
-                          fontFamily: 'Kanit-Light'),),
-                    ));
-                    Navigator.pop(context);
-                  }
-                },
-                icon: Icon(Icons.save),
-                label: Text('Save' ),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.lime,
-                  padding: EdgeInsets.symmetric(horizontal: 55, vertical: 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  textStyle: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                      SizedBox(height: 16),
+      
+                      TextField(
+                        decoration: new InputDecoration(
+                          
+                          enabledBorder: OutlineInputBorder(
+                            
+                            borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                             borderRadius: BorderRadius.all(
+              Radius.circular(10.0)),
+                          ),
+                          hintText: 'Enter your WordCard Topic',
+                        ),
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontFamily: 'Kanit-Light'),
+                        onChanged: (text) {
+                          setState(() {
+                            wordCardTopic = text;
+                          });
+                        },
+                      ),
+                      SizedBox(height: 16),
+                      Text('Favorite Group:'),
+                      SizedBox(height: 8),
+                      ToggleButtons(
+                        children: <Widget>[
+                          Icon(Icons.star_rate_rounded,
+                              color: Color(0xFFFF3377), size: 30),
+                          Icon(Icons.star_rate_rounded,
+                              color: Color(0xFFE53344), size: 30),
+                          Icon(Icons.star_rate_rounded,
+                              color: Color(0xFF33DDAA), size: 30),
+                          Icon(Icons.star_rate_rounded,
+                              color: Color(0xFF3344AA), size: 30),
+                          Icon(Icons.star_rate_rounded,
+                              color: Color(0xFFFFDD00), size: 30),
+                        ],
+                        onPressed: (int index) {
+                          setState(() {
+                            groupSelected[index] = !groupSelected[index];
+      
+                            print(groupSelected);
+                          });
+                        },
+                        isSelected: groupSelected,
+                      ),
+                    ],
                   ),
                 ),
-              )
-            ],
+                SizedBox(height: 16),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    if (wordCardName != '' && wordCardTopic != '') {
+                      insertWordCard();
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text(
+                            'Congret! You have $wordCardName as a new WordCard.',style: TextStyle(
+                        
+                            fontFamily: 'Kanit-Light'),),
+                      ));
+                      Navigator.pop(context);
+                    }
+                  },
+                  icon: Icon(Icons.save),
+                  label: Text('Save' ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.lime,
+                    padding: EdgeInsets.symmetric(horizontal: 55, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    textStyle: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
